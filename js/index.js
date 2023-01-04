88ca60351bc38c09d805d4985b3910830bbcc61b
Joel Herrero <joelv2@gmail.com>
2023-01-04 14:46:05 +0100

    Heading and About sections

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
+//Toggle Mobile Nav
+const hambuergueIcon = document.querySelector('.menu__mobile__hamburguer');
+const menu = document.querySelector('menu');
+
+hambuergueIcon.addEventListener('click', ()=>{
+    menu.classList.toggle("menu__mobile__show");
+})
+
 //Toggle Dark Theme
 const checkbox = document.getElementById('checkbox');
 
 checkbox.addEventListener('change', ()=>{
     document.body.classList.toggle('dark');
+    hambuergueIcon.classList.toggle('dark');
 })
---
+++ js/menu.js
@@ -0,0 +1,23 @@
+const navlist = document.querySelectorAll('.nav__ul li');
+
+
+// document.addEventListener('scroll', () => {
+//     const clientPos = document.getBoundingClientRect();
+//     if (condition) {
+        
+//     }
+// })
+
+
+// navlist.forEach((list) => {
+// 	list.addEventListener('mouseover', () => {
+// 		let position = list.getBoundingClientRect();
+	
+// 		overlay.style.left = (position.x - 20) + 'px';
+	
+// 	});
+// 	list.addEventListener('mouseout', () => {
+// 		overlay.style.left = (initPosition.x - 20) + 'px';
+// 	});
+// });
+
