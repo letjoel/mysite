00fa3753eccd4bc31ce9d07430182cc008bafb6b
Joel Herrero <joelv2@gmail.com>
2023-05-22 15:39:52 +0200

    Offline portfolio updated

--- data/data.js
+++ data/data.js
@@ -1,17 +1,18 @@
 export const localData = {
   projects: [
     {
-      _id:"1",
+      _id: "1",
       name: "Kanban",
       displayName: "Kanban",
-      description: "Kanban board with draggable cards distributed in three columns.",
+      description:
+        "Kanban board with draggable cards distributed in three columns.",
       url: "https://elniu.dev/frontend/whiteboard/",
       repositoryUrl: "https://github.com/letjoel/kanbanboard",
       technologies: ["HTML", "CSS", "JS"],
       image: "https://elniu.dev/assets/images/projects/project1-duo-yellow.png",
     },
     {
-      _id:"2",
+      _id: "2",
       name: "Squad list",
       displayName: "Squad list",
       description: "Filtered list of players on a soccer team.",
@@ -21,27 +22,29 @@ export const localData = {
       image: "https://elniu.dev/assets/images/projects/project2-duo-yellow.png",
     },
     {
-      _id:"3",
+      _id: "3",
       name: "Time Lab",
       displayName: "Time Lab",
-      description: "Countdown, chronometer and current date/time representation.",
+      description:
+        "Countdown, chronometer and current date/time representation.",
       url: "https://elniu.dev/frontend/timelab/",
       repositoryUrl: "https://github.com/letjoel/TimeLab",
       technologies: ["HTML", "CSS", "JS"],
       image: "https://elniu.dev/assets/images/projects/project3-duo-yellow.png",
     },
     {
-      _id:"4",
+      _id: "4",
       name: "InfoAD",
       displayName: "InfoAD",
-      description: "Daily activity report for a PSAP. App connected to a DB. User authentification. Queries by date and file xport ",
+      description:
+        "Daily activity report for a PSAP. App connected to a DB. User authentification. Queries by date and file xport ",
       url: "https://infoad.up.railway.app/",
       repositoryUrl: "https://github.com/letjoel/infoAD",
       technologies: ["HTML", "CSS", "PHP", "MYSQL"],
       image: "https://elniu.dev/assets/images/projects/project4-duo-yellow.png",
     },
     {
-      _id:"5",
+      _id: "5",
       name: "Ecommerce",
       displayName: "Ecommerce",
       description: "Marketplace with a functional javascript shopping cart.",
@@ -51,27 +54,29 @@ export const localData = {
       image: "https://elniu.dev/assets/images/projects/project5-duo-yellow.png",
     },
     {
-      _id:"6",
+      _id: "6",
       name: "Wheel of doom",
       displayName: "Wheel of doom",
-      description: "A game to randomly duel against a list of enemies. It uses localstorage.",
+      description:
+        "A game to randomly duel against a list of enemies. It uses localstorage.",
       url: "https://letjoel.github.io/jokersgame/",
       repositoryUrl: "https://github.com/letjoel/jokersgame",
       technologies: ["HTML", "CSS", "JS"],
       image: "https://elniu.dev/assets/images/projects/project6-duo-yellow.png",
     },
     {
-      _id:"7",
+      _id: "7",
       name: "Hotel",
       displayName: "Hotel",
-      description: "Guest house wordpress site with owned custom booking system.",
+      description:
+        "Guest house wordpress site with owned custom booking system.",
       url: "https://casaruralahora.com",
       repositoryUrl: "#",
       technologies: ["HTML", "CSS", "WORDPRESS", "PHP"],
       image: "https://elniu.dev/assets/images/projects/project7-duo-yellow.png",
     },
     {
-      _id:"8",
+      _id: "8",
       name: "Chat",
       displayName: "Chat",
       description: "Chat using socket.io and express.",
@@ -79,70 +84,93 @@ export const localData = {
       repositoryUrl: "https://github.com/letjoel/chat-socketio",
       technologies: ["SOCKETIO", "NODEJS", "HTML", "CSS"],
       image: "https://elniu.dev/assets/images/projects/project8-duo-yellow.png",
-
     },
     {
-      _id:"9",
+      _id: "9",
       name: "Portfolio",
       displayName: "Portfolio",
-      description: "Made with vanilla javascript and express.js, API endpoint connected to a MongoDB from where the project list is rendered. ",
+      description:
+        "Made with vanilla javascript and express.js, REST API and MongoDB. ",
       url: "https://elniu.dev",
       repositoryUrl: "https://github.com/letjoel/mysite",
       technologies: ["JS", "HTML", "CSS", "NODEJS", "MONGODB"],
       image: "https://elniu.dev/assets/images/projects/project9-duo-yellow.png",
-
     },
     {
-      _id:"10",
+      _id: "10",
       name: "Rest API",
       displayName: "Rest API",
-      description: "API developed with NestJs, TypeScript, Swagger and MongoDB. User auth by roles and custom decorators.",
+      description:
+        "API developed with NestJs, TypeScript, Swagger and MongoDB. User auth by roles and custom decorators.",
       url: "https://blackbackersrecette.up.railway.app/",
       repositoryUrl: "https://github.com/letjoel/api_social-recipies",
       technologies: ["NESTJS", "TYPESCRIPT", "MONGODB"],
-      image: "https://elniu.dev/assets/images/projects/project10-duo-yellow.png",
-   
+      image:
+        "https://elniu.dev/assets/images/projects/project10-duo-yellow.png",
     },
     {
-      _id:"11",
+      _id: "11",
       name: "React to do",
       displayName: "React to do",
       description: "Single page app (SAP) developed with ReactJs. ",
       url: "https://react-to-do-beta.vercel.app/",
       repositoryUrl: "https://github.com/letjoel/react-soundwave",
       technologies: ["REACT", "TYPESCRIPT", "CSS"],
-      image: "https://elniu.dev/assets/images/projects/project11-duo-yellow.png",
+      image:
+        "https://elniu.dev/assets/images/projects/project11-duo-yellow.png",
     },
     {
-      _id:"12",
+      _id: "12",
       name: "React Soundwave",
       displayName: "React Soundwave",
-      description: "React SPA styled with css modules and Material UI. Mockup layout provided.",
+      description:
+        "React SPA styled with css modules and Material UI. Mockup layout provided.",
       url: "https://react-soundwave.netlify.app/",
       repositoryUrl: "https://github.com/letjoel/react-soundwave/tree/mui",
       technologies: ["REACT", "TYPESCRIPT", "CSS", "MATERIALUI"],
-      image: "https://elniu.dev/assets/images/projects/project12-duo-yellow.png",
-
+      image:
+        "https://elniu.dev/assets/images/projects/project12-duo-yellow.png",
     },
-    { 
-      _id:"13",          
-      "name": "Real Dream Voices",
-      "displayName": "Real Dream Voices",
-      "description": "Marketplace created with React",
-      "url": "https://realdreamvoices.vercel.app/",
-      "repositoryUrl": "https://github.com/24teamhackaton/marketplace",
-      "technologies": ["REACT", "CSS"],
-      "image": "https://elniu.dev/assets/images/projects/project13-duo-yellow.png"
+    {
+      _id: "13",
+      name: "Real Dream Voices",
+      displayName: "Real Dream Voices",
+      description: "Marketplace created with React",
+      url: "https://realdreamvoices.vercel.app/",
+      repositoryUrl: "https://github.com/24teamhackaton/marketplace",
+      technologies: ["REACT", "CSS"],
+      image:
+        "https://elniu.dev/assets/images/projects/project13-duo-yellow.png",
     },
-    { 
-      _id:"14",  
-      "name": "Imagine",
-      "displayName": "Imagine",
-      "description": "React App to upload and edit your favourite images. Backend with API Rest and MongoDB.",
-      "url": "https://imagineation.netlify.app/",
-      "repositoryUrl": "https://github.com/letjoel/front-myimages#readme",
-      "technologies": ["REACT", "NESTJS", "TYPESCRIPT","MONGODB", "CSS"],
-      "image": "https://elniu.dev/assets/images/projects/project2-duo-yellow.png"
-    }
-  ]
-}
+    {
+      _id: "14",
+      name: "Imagine",
+      displayName: "Imagine",
+      description:
+        "React App to upload and edit your favourite images. Backend with API Rest and MongoDB.",
+      url: "https://imagineation.netlify.app/",
+      repositoryUrl: "https://github.com/letjoel/front-myimages#readme",
+      technologies: ["REACT", "NESTJS", "TYPESCRIPT", "MONGODB", "CSS"],
+      image:
+        "https://elniu.dev/assets/images/projects/project14-duo-yellow.png",
+    },
+    {
+      _id: "15",
+      name: "Library of Things",
+      displayName: "Library of Things",
+      description:
+        "Lending app enabling users to access uncommon objects for their homes. React, MaterialUI, Nestjs, MySQL.",
+      repositoryUrl: "https://github.com/cosateca/cosatk-front#readme",
+      technologies: [
+        "REACT",
+        "MATERIALUI",
+        "NESTJS",
+        "TYPESCRIPT",
+        "MYSQL",
+        "CSS",
+      ],
+      image:
+        "https://elniu.dev/assets/images/projects/project15-duo-yellow.png",
+    },
+  ],
+};
